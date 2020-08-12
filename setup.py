cfa7c3687ca568e7f239ad973fc22ae805590408
Brikwerk <reece@brikwerk.com>
2020-08-11 20:28:43 -0700

    Added blessed and pynput for TUI

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setuptools.setup(
         "Flask>=1.1.2",
         "Flask-SocketIO>=4.3.0",
         "eventlet>=0.25.2",
+        "blessed>=1.17.9",
+        "pynput>=1.6.8",
     ],
     extra_require={
         "dev": [
