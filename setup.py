2fd4dc531f5471272a39a31f0367ea7e605e67c5
Brikwerk <reece@brikwerk.com>
2021-01-01 14:55:54 -0800

    Pinned versions and updated socket.io dependencies

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     include_package_data=True,
     long_description_content_type="text/markdown",
     install_requires=[
-        "dbus-python>=1.2.16",
-        "Flask>=1.1.2",
-        "Flask-SocketIO>=4.3.0",
-        "eventlet>=0.25.2",
-        "blessed>=1.17.9",
-        "pynput>=1.6.8",
-        "psutil>=5.5.1",
+        "dbus-python==1.2.16",
+        "Flask==1.1.2",
+        "Flask-SocketIO==5.0.1",
+        "eventlet==0.25.2",
+        "blessed==1.17.9",
+        "pynput==1.6.8",
+        "psutil==5.5.1",
     ],
     extra_require={
         "dev": [
