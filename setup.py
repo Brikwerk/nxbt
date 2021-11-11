eeb7e08f18a7dca9d3a9711fd9242b483e9318a2
inorizushi <inorizushi@gmail.com>
2021-11-10 21:54:43 -0600

    Updated dependancies in setup.py
    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     include_package_data=True,
     long_description_content_type="text/markdown",
     install_requires=[
-        "dbus-python==1.2.16",
-        "Flask==1.1.2",
-        "Flask-SocketIO==5.0.1",
-        "eventlet==0.31.0",
+        "Flask==2.1.3",
+        "Flask-SocketIO==4.3.1",
+        "eventlet==0.33.3",
         "blessed==1.17.10",
         "pynput==1.7.1",
         "psutil==5.6.6",
         "cryptography==3.3.2",
+        "Werkzeug==2.0.3",
     ],
     extra_require={
         "dev": [
