b44efb3b113e7ec34726d828788055225f432e49
Jazzmax <mappett.show@gmail.com>
2023-01-02 19:13:15 +0000

    Bump dependencies: Flask 2.0.0 Added handling of permanent bluetooth service override

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         "dbus-python==1.2.16",
-        "Flask==1.1.2",
-        "Flask-SocketIO==5.0.1",
-        "eventlet==0.31.0",
+        "Flask~=2.0.0",
+        "Flask-SocketIO~=5.1.0",
+        "eventlet~=0.33.0",
         "blessed==1.17.10",
         "pynput==1.7.1",
         "psutil==5.6.6",
