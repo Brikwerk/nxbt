538bbfb7a87bf6d740464f916184ba9a3aa76d0b
Dummydude <96631251+Dummydud3@users.noreply.github.com>
2023-06-06 15:52:36 -0400

    Updated webapp dependancies in setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,16 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         "dbus-python==1.2.16",
-        "Flask==1.1.2",
-        "Flask-SocketIO==5.0.1",
-        "eventlet==0.31.0",
+        "Flask==2.1.3",
+        "Flask-SocketIO==4.3.1",
+        "eventlet==0.33.3",
         "blessed==1.17.10",
         "pynput==1.7.1",
         "psutil==5.6.6",
         "cryptography==3.3.2",
+        "jinja2==3.0.3",
+        "itsdangerous==2.0.1",
+        "Werkzeug==2.0.3",
     ],
     extra_require={
         "dev": [
